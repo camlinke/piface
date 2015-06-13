332fb60a33f6119921f91d3bb1b70802ea7718fe
Your Name <cam.linke@gmail.com>
2015-06-13 10:44:14 -0600

    stuff

---
+++ piface.py
@@ -0,0 +1,19 @@
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
+
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
